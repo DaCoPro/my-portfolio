c33f9a1567d190603fc334fcf5ae5afa87f29e19
DaCoPro <comerbdavid@gmail.com>
2021-02-09 11:55:18 -0800

    Add contact page.

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,3 +1,14 @@
+import './Contact.css';
+
 export default function Contact () {
-    return <h1>Contact</h1>
+    return (
+        <main className="Contact">
+            <h1>Contact Me:</h1>
+            <h2>comerbdavid@gmail.com</h2>
+            <div className="CMLinks">
+                <a href="https://www.linkedin.com/in/d-comer/"><h4>On Linked In</h4></a>
+                <a href="https://github.com/DaCoPro"><h4>On Github</h4></a>
+            </div>
+        </main>
+    );
 }
