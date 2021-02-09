a10096f212703b0fd8fed151d42a7a89462de542
DaCoPro <comerbdavid@gmail.com>
2021-02-09 11:43:03 -0800

    Add Portfolio & Resume.

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -1,3 +1,43 @@
+import './Portfolio.css';
+import mernphoto from './MERNflash.png';
+import squirrelphoto from './zs.png';
+import uspolling from './uspoll.png';
+import minesweeper from './minesweeper.png';
+
 export default function Portfolio () {
-    return <h1>Portfolio</h1>
+    return ( 
+        <main className="Portfolio">
+            <div className="WholeDoc">
+                <div>
+                    <h1>The Flash App</h1>
+                    <h4>A MERN stack application built for users who'd like to study smarter, not harder. The Flash App 
+                        is a platform for users to create decks of flashcards, and informs them what they should study.</h4>
+                    <h4>Build with JS, HTML, CSS, MongoDB, Express, Node, & React.</h4>
+                    <a href="https://github.com/DaCoPro/MERN-Flashcards"><img className="PortPic" src={mernphoto} alt=""/></a>
+                </div>
+                <div>
+                    <h1>US Polling</h1>
+                    <h4>A Polling application built to let a user create poll questions and query other users. The 
+                        results are displayed in a graphical bar-chart. 
+                    </h4>
+                    <h4>Build with Python, Django, CSS, HTML.</h4>
+                    <a href="https://github.com/DaCoPro/US-Polling"><img className="PortPic" src={uspolling} alt=""/></a>
+                </div>
+                <div>
+                    <h1>Zen Squirrel</h1>
+                    <h4>A project management application with built in Pomodoro Timer. 
+                    </h4>
+                    <h4>Build with JS, Express, Node, CSS, HTML.</h4>
+                    <a href="https://github.com/DaCoPro/Pomodoro-proj-mgnt"><img className="PortPic" src={squirrelphoto} alt=""/></a>
+                </div>
+                <div>
+                    <h1>MineSweeper</h1>
+                    <h4>An interactive browser game built to learn the dynamics of MVC separation of concerns. Fully functional. 
+                    </h4>
+                    <h4>Build with JS, CSS, HTML.</h4>
+                    <a href="https://github.com/DaCoPro/minesweeper"><img className="PortPic" src={minesweeper} alt=""/></a>
+                </div>
+            </div>
+        </main>
+    );
 }
--- src/Pages/Resume/Resume.jsx
+++ src/Pages/Resume/Resume.jsx
@@ -1,3 +1,36 @@
+import './Resume.css';
+
 export default function Resume () {
-    return <h1>Resume</h1>
+    return ( 
+        <main className="Resume">
+            <div className="WholeDoc">
+                <div className="Experience">
+                    <h1>Experience</h1>
+                    <h2>Software Engineering</h2>
+                    <h4>General Assembly</h4>
+                    <ul>
+                        <li>From October of 2020 through February of 2021, I spent 500 hours learning and utilizing full-stack engineering skills. </li>
+                        <li>Areas of study included web basics, HTML - CSS - JS, Express Applications, Django/Python Applications, and MERN Stack. </li>
+                        <li>Accumulated a collaborative and individual portfolio of work.</li>
+                    </ul>
+                    <h2>Client Services Advisor</h2>
+                    <h4>Summit Bank</h4>
+                    <ul>
+                        <li>From November of 2018 through October of 2020. </li>
+                        <li>I continually increased my responsability. I balanced all branch cash and was undergoing training for new-accounts work.</li>
+                    </ul>
+                </div>
+                <div className="Education">
+                    <h1>Education</h1>
+                    <h2>California State University, Fresno</h2>
+                    <h4>Bachelors in Economics </h4>
+                    <ul>
+                        <li>Minor in Philosophy</li>
+                        <li>Dean's List</li>
+                    </ul>
+                </div>
+                <a href="">Download My Resume</a>
+            </div>
+        </main>
+    );
 }
