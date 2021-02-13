f223ef06c04c7700afa9fd6660bb3dee3a063870
DaCoPro <comerbdavid@gmail.com>
2021-02-13 13:53:35 -0800

    Change styling.

--- src/Pages/Resume/Resume.jsx
+++ src/Pages/Resume/Resume.jsx
@@ -5,31 +5,46 @@ export default function Resume () {
         <main className="Resume">
             <div className="WholeDoc">
                 <div className="Experience">
-                    <h1>Experience</h1>
-                    <h2>Software Engineering</h2>
-                    <h4>General Assembly</h4>
+                    <h1>EXPERIENCE</h1>
+                    <span><h2 className="Heading">SOFTWARE ENGINEERING</h2><h4 className="Date" >October 2020 - February 2021</h4></span>
+                    <h3 className="Info">GENERAL ASSEMBLY</h3>
                     <ul>
                         <li>From October of 2020 through February of 2021, I spent 500 hours learning and utilizing full-stack engineering skills. </li>
                         <li>Areas of study included web basics, HTML - CSS - JS, Express Applications, Django/Python Applications, and MERN Stack. </li>
                         <li>Accumulated a collaborative and individual portfolio of work.</li>
                     </ul>
-                    <h2>Client Services Advisor</h2>
-                    <h4>Summit Bank</h4>
+                    <span><h2 className="Heading">BANKING OPERATIONS</h2><h4 className="Date" >November 2018 - October 2020</h4></span>
+                    <h3 className="Info">SUMMIT BANK</h3>
                     <ul>
                         <li>From November of 2018 through October of 2020. </li>
                         <li>I continually increased my responsability. I balanced all branch cash and was undergoing training for new-accounts work.</li>
                     </ul>
                 </div>
+                <br/>
+                <hr/>
+                <br/>
                 <div className="Education">
-                    <h1>Education</h1>
-                    <h2>California State University, Fresno</h2>
-                    <h4>Bachelors in Economics </h4>
+                    <h1>EDUCATION</h1>
+                    <span><h2 className="Heading">BACHELORS IN ECONOMICS</h2><h4 className="Date" >August 2016 -  June 2018</h4></span>
+                    <h4 className="Info">CALIFORNIA STATE UNIVERSITY, FRESNO</h4>
                     <ul>
                         <li>Minor in Philosophy</li>
                         <li>Dean's List</li>
                     </ul>
                 </div>
-                <a href="">Download My Resume</a>
+                <br/>
+                <hr/>
+                <br/>
+                <div className="Education">
+                    <h1>EDUCATION</h1>
+                    <span><h2 className="Heading">BACHELORS IN ECONOMICS</h2><h4 className="Date" >August 2016 -  June 2018</h4></span>
+                    <h4 className="Info">CALIFORNIA STATE UNIVERSITY, FRESNO</h4>
+                    <ul>
+                        <li>Minor in Philosophy</li>
+                        <li>Dean's List</li>
+                    </ul>
+                </div>
+                
             </div>
         </main>
     );
